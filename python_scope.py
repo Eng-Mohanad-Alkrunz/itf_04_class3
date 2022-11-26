12412b15a12ced42ab6c9eadeca4804080ba4c87
mohanad-alkrunz <muhanadalkrunz@gmail.com>
2022-11-26 19:45:41 +0200

    Second Commit

--- python_scope.py
+++ python_scope.py
@@ -4,12 +4,13 @@ a = 40
 
 def outer_function():
     c = 30 # local variable
+    d = 50
     # global a
     print("a from outer_function before modified",globals()['a'])
     globals()['a'] = 60
     a = 20
     builtins.print()
-    
+
     def inner_function():
         b = 30
         a = 30
